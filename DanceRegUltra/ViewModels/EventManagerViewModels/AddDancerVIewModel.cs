cb5b9e09a3b7f92139d3731cbef975a9513142b9
Alex Coreman <scorpionsik@gmail.com>
2020-08-28 17:24:32 +0300

    Start edit logic for adding Dancers

---
+++ DanceRegUltra/ViewModels/EventManagerViewModels/AddDancerVIewModel.cs
@@ -0,0 +1,41 @@
+﻿using CoreWPF.MVVM;
+using DanceRegUltra.Models;
+using DanceRegUltra.Models.Categories;
+using DanceRegUltra.Utilites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanceRegUltra.ViewModels.EventManagerViewModels
+{
+    public class AddDancerViewModel : ViewModel
+    {
+        private int EventID;
+        
+        internal FindDancer FindList { get; private set; }
+
+        private MemberDancer dancerInWork;
+        public MemberDancer DancerInWork
+        {
+            get => this.dancerInWork;
+            private set
+            {
+                this.dancerInWork = value;
+                this.OnPropertyChanged("DancerInWork");
+            }
+        }
+
+        public List<int> Leagues { get; private set; }
+        public List<int> Ages { get; private set; }
+        public List<IdCheck> Styles { get; private set; }
+
+        public AddDancerViewModel(int event_id)
+        {
+            this.EventID = event_id;
+            this.DancerInWork = new MemberDancer(event_id, -1, "", "");
+        }
+  
+    }
+}
---
+++ DanceRegUltra/Views/EventManagerViews/AddDancerView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DanceRegUltra.ViewModels.EventManagerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DanceRegUltra.Views.EventManagerViews
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDancerView.xaml
+    /// </summary>
+    public partial class AddDancerView : Window
+    {
+        public AddDancerView(int event_id)
+        {
+            InitializeComponent();
+            this.DataContext = new AddDancerViewModel(event_id);
+        }
+    }
+}
